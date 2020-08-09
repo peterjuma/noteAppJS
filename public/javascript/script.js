2897197bd1f9af50a3f80b918fd1f2710c4f4035
Peter Juma <wanyonyi.p@gmail.com>
2020-08-09 22:03:38 +0300

    Show "Created at"

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -64,6 +64,8 @@ class Notes {
         objectStore.createIndex('title', 'title', { unique: false });
         objectStore.createIndex('body', 'body', { unique: false });
         objectStore.createIndex('noteid', 'noteid', { unique: true });
+        objectStore.createIndex('created_at', 'created_at', { unique: true });
+        objectStore.createIndex('updated_at', 'updated_at', { unique: true });
         db.close();
     };
     }
@@ -105,26 +107,33 @@ const queryDB = () => {
         var cust = tx.objectStore("notes")
         var request = cust.openCursor()
         request.onsuccess = (e) => {
-            var cursor = e.target.result
+            var cursor = e.target.result            
             if (cursor) {
             //`{noteid: ${cursor.key}, title: ${cursor.value.title}, body: ${cursor.value.body}}`  
             // contenteditable
+                var date_diff = countDown(cursor.value.created_at)
+                var ago = []
+                date_diff.days > 0 ? ago[0] = (date_diff.days + "d") : date_diff.hours > 0 ? ago[1] = (date_diff.hours + "h") : date_diff.minutes > 0 ? ago[2] = (date_diff.seconds + "m") : ago[2] = "now"
+                console.log(ago)
                 html = `<div class="column note" id="${cursor.key}" onclick='showNote(this)'>
                             <h2>${marked(cursor.value.title)}</h2>
+                             <caption>Created ${ago[0]||""} ${ago[1]||""} ${ago[2]||""} ago</caption>
                         </div>`;
                 notesGrid.innerHTML += html;
+                noteSelect() 
                 cursor.continue()
             }
         }
-    };
+    }
 }
 
 let btnAction = document.getElementsByClassName("btnnote")
 var editBox = document.getElementById("editor")
 
-// Show notes when grid box clicked
+// Show note when grid box clicked
 function showNote(notediv){
     var noteid = notediv.id || notediv.name || notediv;
+    // Highlight note when clicked
     var connection = indexedDB.open(DBNAME);
     connection.onsuccess = function () {
         db = connection.result;
@@ -146,17 +155,28 @@ function showNote(notediv){
                 // document.getElementById("saveBtn").disabled = true;
             } else{}
         }
-    }       
+    } 
+    // document.getElementById(notediv.id).classList.add("active") 
+}
+
+function noteSelect(){
+    var noteList = document.getElementsByClassName("note");
+    for (var i = 0; i < noteList.length; i++) {
+        noteList[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
 }
 
 var editPad = document.getElementById("editpad")
 var newNote = document.getElementById("addBtn")
-
 newNote.addEventListener("click", () => {
     html = `<div name="" class="editor" id="editpad" contenteditable="false">
                 <input name="title" type="text" id="title" placeholder="Note Title">
                 <textarea name="notebody" cols="30" rows="10" id="notebody" placeholder="Body..."></textarea>
-                <button onclick='cancelEdit("")' class="btn btnnote" style="float: right; background-color: #ddd;" onMouseOut="this.style.color='crimson'" onMouseOver="this.style.color='green'"><i class="fas fa-window-close"></i> Cancel</button>
+                <button onclick='cancelEdit("")' class="btn btnnote" style="float: right; background-color: #ddd;" onMouseOut="this.style.color='crimson'" onMouseOver="this.style.color='green'"><i class="fas fa-window-close fa-lg"></i> Cancel</button>
                 <button class="btn" id="saveBtn" style="float: left;" onclick="save()"><i class="fas fa-save fa-lg"></i> Save</button>
             </div>`
     editBox.innerHTML = html;
@@ -184,19 +204,37 @@ function deleteNote(notediv) {
     }
 }
 
+
+
+function countDown(epoch_timestamp) {
+    var now = new Date().getTime();
+    // Find the distance between now and the count down date
+    var distance = now - epoch_timestamp * 1000;
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    dateJson = {
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    }
+    return dateJson
+}
 // Get new note values from the UI
 function save() {
-    function genID(d) {
-        return Math.floor(d / 1000);
-    }
     var noteTitle = document.getElementById("title").value
     var noteBody = document.getElementById("notebody").value
-    var d = new Date()
-    var id = genID(d); 
+    var id = Math.round(Date.now() / 1000)  
     const note = {
         noteid: id.toString(),
         title: noteTitle,
-        body: noteBody
+        body: noteBody,
+        created_at: id.toString(),
+        updated_at: ""
     };
     console.log(note)
     addNote(note)
@@ -225,30 +263,18 @@ function update(editdiv) {
     var id = editdiv.name
     var noteTitle = document.getElementById("title").value
     var noteBody = document.getElementById("notebody").value
+    var updated_at = Math.round(Date.now() / 1000)  
     const note = {
         noteid: id.toString(),
         title: noteTitle,
-        body: noteBody
+        body: noteBody,
+        created_at: id.toString(),
+        updated_at: updated_at.toString()
     };
     console.log(note)
     updateNote(note)
 }
 
-// Update note in the database
-function updateNote(note){
-    var connection = indexedDB.open(DBNAME);
-    connection.onsuccess = function () {
-        db = connection.result;
-        var tx = db.transaction('notes', "readwrite")
-        var store = tx.objectStore("notes")
-        store.put(note);
-        tx.oncomplete = function () {
-            console.log('Note updated' + note);
-            document.getElementById(note.noteid).style.display = "none"
-        }
-    }
-    getNote(note.noteid)
-}
 
 function cancelEdit(notediv){
     if(notediv){
@@ -259,7 +285,7 @@ function cancelEdit(notediv){
     }
 }
 
-// Get single not to display in the list and details areas
+// Get single note to display in the list and details areas
 function getNote(noteid) {
     var connection = indexedDB.open(DBNAME);
     connection.onsuccess = function () {
@@ -289,6 +315,22 @@ function getNote(noteid) {
     }
 }
 
+// Update note in the database
+function updateNote(note) {
+    var connection = indexedDB.open(DBNAME);
+    connection.onsuccess = function () {
+        db = connection.result;
+        var tx = db.transaction('notes', "readwrite")
+        var store = tx.objectStore("notes")
+        store.put(note);
+        tx.oncomplete = function () {
+            console.log('Note updated' + note);
+            document.getElementById(note.noteid).style.display = "none"
+        }
+    }
+    getNote(note.noteid)
+}
+
 const turndownService = new TurndownService();
 
 // Get single note for editing
@@ -307,8 +349,8 @@ function editNote(notediv) {
                 html = `<div name=${matching.noteid} class="editor" id="editpad">
                             <input name="title" type="text" id="title">
                             <textarea style="margin-top: 20px;" name="notebody" id="notebody"></textarea>
-                            <button onclick='update(this)' name="${matching.noteid}" class="btn btnnote" style="float: left; background-color: #ddd;" onMouseOut="this.style.color='crimson'" onMouseOver="this.style.color='green'"><i class="fa fa-save fa-lg"></i> Update</button>
-                            <button onclick='cancelEdit(this)' name="${matching.noteid}" class="btn btnnote" style="float: right; background-color: #ddd;" onMouseOut="this.style.color='crimson'" onMouseOver="this.style.color='green'"><i class="fas fa-window-close"></i> Cancel</button>
+                            <button onclick='update(this)' name="${matching.noteid}" class="btn btnnote" style="float: left; background-color: #ddd;" onMouseOut="this.style.color='crimson'" onMouseOver="this.style.color='green'"><i class="fa fa-check fa-lg" aria-hidden="true"></i> OK</button>
+                            <button onclick='cancelEdit(this)' name="${matching.noteid}" class="btn btnnote" style="float: right; background-color: #ddd;" onMouseOut="this.style.color='crimson'" onMouseOver="this.style.color='green'"><i class="fas fa-window-close fa-lg"></i> Cancel</button>
                         </div>`
                 editBox.innerHTML = html;
                 document.getElementById("title").value = turndownService.turndown(marked(matching.title));
